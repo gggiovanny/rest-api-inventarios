01403c855d2bc6b69a5d578db9d2799741681e45
gggiovanny <giovanny.gonzalez.19@gmail.com>
2019-11-08 17:56:05 -0600

    Corregido bug que desplegaba auditorias repetidas

--- app/Http/Controllers/ActivosController.php
+++ app/Http/Controllers/ActivosController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Activo;
+use App\Models\Auditoria;
 use Illuminate\Support\Facades\DB;
 
 class ActivosController extends Controller
@@ -15,144 +16,134 @@ class ActivosController extends Controller
      */
     public function index(Request $request)
     {
-       AuthController::validateCredentials($request);
+        AuthController::validateCredentials($request);
+        $query = null;
 
-       $query = null;
-
-        /** Mostrar activos sin ubicacion */
-        $mostrarSinUbicacion = $request->input('sin_ubicacion') == 'false' ? null : $request->input('sin_ubicacion');
         /** Mostrar valor guardado en auditoria actual */
         $auditoria_actual = $request->input('auditoria_actual') ? $request->input('auditoria_actual') : '0';
         /** Paginado */
         $page_size = $request->input('page_size') ? $request->input('page_size') : self::$PAGE_SIZE_DEFAULT;
         $page = $request->input('page') ? $request->input('page') : 1;
-        /** Filtros */         
+        /** Filtros */
         $idEmpresa = $request->input('empresa');
         $idDepartamento = $request->input('departamento');
         $idClasificacion = $request->input('clasificacion');
         $existencia = $request->input('existencia');
-        if(($existencia === '0' || $existencia === 'false' ) && !is_null($existencia)) { $existencia = -1; } // Se pone en -1 porque 0 se toma como null en el when
+        if (($existencia === '0' || $existencia === 'false') && !is_null($existencia)) {
+            $existencia = -1;
+        } // Se pone en -1 porque 0 se toma como null en el when
         /** Busqueda */
         $search = $request->input('search');
         /** Ordenamiento */
         $sort_by = $request->input('sort_by') ? $request->input('sort_by') : 'idActivoFijo';
         $sort_order = $request->input('sort_order') ? $request->input('sort_order') : 'asc';
 
-        if($mostrarSinUbicacion)
-        {
-            $query = Activo::leftJoin('movimiento_detalle AS MVD', 'activosfijos.idActivoFijo', 'MVD.idActivoFijo')
-                        ->leftJoin('auditorias_activofijos as AUA', 'activosfijos.idActivoFijo', 'AUA.idActivoFijo')
-                        ->leftJoin('auditorias as AU', 'AUA.idAuditoria', 'AU.idAuditoria')
-                        ->select(   'activosfijos.idActivoFijo'
-                                    ,'activosfijos.descripcion'
-                                    ,DB::raw('CONVERT(AUA.existencia, SIGNED) AS existencia_guardada')
-                                    ,DB::raw('(select existencia from auditorias_activofijos where idAuditoria = '.$auditoria_actual.' and idActivoFijo = activosfijos.idActivoFijo) as existencia_actual')
-                                    ,'AU.fechaGuardada AS fecha_existencia'
-	                                ,'AUA.idAuditoria as id_auditoria_existencia'
-                                    ,'activosfijos.idClasificacion'
-                                )
-                        ->where('activosfijos.estatus', 'false')
-                        ->whereNull('MVD.idMovimientoDetalle')
-                        ->when($idClasificacion, function($ifwhere) use ($idClasificacion) {
-                            return $ifwhere->where('ACT.idClasificacion', $idClasificacion); })
-                        ->when($search, function($ifwhere) use ($search) {
-                            return $ifwhere->where('activosfijos.descripcion', 'like', '%'.$search.'%'); })
+        /** Verifiacion de existencia de los campos obligatorios */
+        if( is_null($auditoria_actual) || $auditoria_actual == "0") {
+            return self::warningNoParameters();
+        }
 
-                        ->orderBy($sort_by, $sort_order)
-                        ->skip(($page-1)*$page_size)
-                        ->take($page_size)
-                        ->get()
-                        ;
-        }
-        else
-        {
-            $query = DB::table('movimiento_detalle AS MVD')
-                        ->join('activosfijos AS ACT', 'MVD.idActivoFijo', 'ACT.idActivoFijo')
-                        ->join('movimientos AS MV', 'MVD.idMovimiento', 'MV.idMovimiento')
-                        ->join('departamentos AS DEP', 'MV.destino', 'DEP.idDepartamento')
-                        ->join('empresas AS EMP', 'DEP.idEmpresa', 'EMP.idEmpresa')
-                        ->leftJoin('auditorias_activofijos as AUA', 'MVD.idActivoFijo', 'AUA.idActivoFijo')
-                        ->leftJoin('auditorias as AU', 'AUA.idAuditoria', 'AU.idAuditoria')
-                        ->select(   'MVD.idActivoFijo'
-                                    ,'ACT.descripcion'
-                                    ,DB::raw('CONVERT(AUA.existencia, SIGNED) AS existencia_guardada')
-                                    ,DB::raw('(select existencia from auditorias_activofijos where idAuditoria = '.$auditoria_actual.' and idActivoFijo = MVD.idActivoFijo) as "existencia_actual"')
-                                    ,'AU.fechaGuardada AS fecha_existencia'
-                                    ,'AUA.idAuditoria as id_auditoria_existencia'
-                                    ,'AU.idUser AS auditoria_autor'
-                                    ,'ACT.idClasificacion'
-                                    ,'DEP.idDepartamento'
-                                    ,'EMP.idEmpresa'
-                                    ,'MV.fecha_acepta AS ultimo_movimiento'	
-                                )
-                        ->where('ACT.estatus', 'false')
-                        ->whereRaw('MV.fecha_acepta =	(
-                            select MAX(m.fecha_acepta)
-                            from movimientos m
-                            inner join movimiento_detalle md
-                                on m.idMovimiento = md.idMovimiento
-                            where md.idActivoFijo = MVD.idActivoFijo
-                            )')
-                        ->whereRaw('(
-                            CASE
-                                WHEN (	select count(*)
-                                        from auditorias_activofijos aa
-                                        inner join auditorias a
-                                            on aa.idAuditoria = a.idAuditoria
-                                        where a.fechaGuardada is not null
-                                        and aa.idActivoFijo = MVD.idActivoFijo ) >= 1
-                                THEN AU.fechaGuardada =	(
-                                                        select MAX(a.fechaGuardada)
-                                                        from auditorias_activofijos aa
-                                                        inner join auditorias a
-                                                            on aa.idAuditoria = a.idAuditoria
-                                                        where aa.idActivoFijo = MVD.idActivoFijo
+        $query = DB::table('movimiento_detalle AS MVD')
+            ->join('activosfijos AS ACT', 'MVD.idActivoFijo', 'ACT.idActivoFijo')
+            ->join('movimientos AS MV', 'MVD.idMovimiento', 'MV.idMovimiento')
+            ->join('departamentos AS DEP', 'MV.destino', 'DEP.idDepartamento')
+            ->join('empresas AS EMP', 'DEP.idEmpresa', 'EMP.idEmpresa')
+            ->leftJoin('auditorias_activofijos as AUA', 'MVD.idActivoFijo', 'AUA.idActivoFijo')
+            ->leftJoin('auditorias as AU', 'AUA.idAuditoria', 'AU.idAuditoria')
+            ->select(
+                'MVD.idActivoFijo',
+                'ACT.descripcion',
+                DB::raw('CASE WHEN AU.fechaGuardada IS NULL THEN NULL ELSE CONVERT(AUA.existencia, SIGNED) END AS existencia_guardada'),
+                DB::raw('(select existencia from auditorias_activofijos where idAuditoria = ' . $auditoria_actual . ' and idActivoFijo = MVD.idActivoFijo) as "existencia_actual"'),
+                'AU.fechaGuardada AS fecha_existencia',
+                'AUA.idAuditoria as id_auditoria_existencia',
+                'AU.idUser AS auditoria_autor',
+                'ACT.idClasificacion',
+                'DEP.idDepartamento',
+                'EMP.idEmpresa',
+                'MV.fecha_acepta AS ultimo_movimiento'
+            )
+            ->where('ACT.estatus', 'false')
+            ->whereRaw('MV.fecha_acepta =	(
+                        select MAX(m.fecha_acepta)
+                        from movimientos m
+                        inner join movimiento_detalle md
+                            on m.idMovimiento = md.idMovimiento
+                        where md.idActivoFijo = MVD.idActivoFijo
+                        )')
+            ->whereRaw('(
+                        CASE
+                            WHEN (	select count(*)
+                                    from auditorias_activofijos aa
+                                    inner join auditorias a
+                                        on aa.idAuditoria = a.idAuditoria
+                                    where a.fechaGuardada is not null
+                                    and aa.idActivoFijo = MVD.idActivoFijo ) >= 1
+                            THEN AU.fechaGuardada =	(
+                                                    select MAX(a.fechaGuardada)
+                                                    from auditorias_activofijos aa
+                                                    inner join auditorias a
+                                                        on aa.idAuditoria = a.idAuditoria
+                                                    where aa.idActivoFijo = MVD.idActivoFijo
+                
+                                                    )
+                            ELSE    CASE
+                                        WHEN AUA.idAuditoria IS NOT NULL
+                                        THEN AUA.idAuditoria = ' . $auditoria_actual . '
+                                        ELSE 1=1
+                                    END								
+                        END			
+                    ) ')
                     
-                                                        )
-                                ELSE AU.fechaGuardada is null								
-                            END			
-                        ) ')
-                        ->when($idEmpresa, function($ifwhere) use ($idEmpresa) {
-                            return $ifwhere->where('EMP.idEmpresa', $idEmpresa); })
-                        ->when($idDepartamento, function($ifwhere) use ($idDepartamento) {
-                            return $ifwhere->where('DEP.idDepartamento', $idDepartamento); })
-                        ->when($idClasificacion, function($ifwhere) use ($idClasificacion) {
-                            return $ifwhere->where('ACT.idClasificacion', $idClasificacion); })
-                        ->when($search, function($ifwhere) use ($search) {
-                            return $ifwhere->where('ACT.descripcion', 'like', '%'.$search.'%'); })
-                        ->when($existencia, function($ifwhere) use ($existencia) {
-                            if($existencia == -1) {
-                                return $ifwhere->whereNull('AUA.existencia'); 
-                            } else {
-                                return $ifwhere->whereNotNull('AUA.existencia')->whereNotNull('AU.fechaGuardada'); 
-                            }
-                        })
-                        
-                        ->orderBy($sort_by, $sort_order)
-                        ->skip(($page-1)*$page_size)
-                        ->take($page_size)
-                        ->get()
-                        ;
-        }
+            ->where('EMP.idEmpresa', Auditoria::find($auditoria_actual)->idEmpresa)
+            ->where('DEP.idDepartamento', Auditoria::find($auditoria_actual)->idDepartamento)
+            ->where('ACT.idClasificacion', Auditoria::find($auditoria_actual)->idClasificacion)
+            
+            ->when($idEmpresa, function ($ifwhere) use ($idEmpresa) {
+                return $ifwhere->where('EMP.idEmpresa', $idEmpresa);
+            })
+            ->when($idDepartamento, function ($ifwhere) use ($idDepartamento) {
+                return $ifwhere->where('DEP.idDepartamento', $idDepartamento);
+            })
+            ->when($idClasificacion, function ($ifwhere) use ($idClasificacion) {
+                return $ifwhere->where('ACT.idClasificacion', $idClasificacion);
+            })
+            ->when($search, function ($ifwhere) use ($search) {
+                return $ifwhere->where('ACT.descripcion', 'like', '%' . $search . '%');
+            })
+            ->when($existencia, function ($ifwhere) use ($existencia) {
+                if ($existencia == -1) {
+                    return $ifwhere->whereNull('AUA.existencia');
+                } else {
+                    return $ifwhere->whereNotNull('AUA.existencia')->whereNotNull('AU.fechaGuardada');
+                }
+            })
+                       
+
+            ->orderBy($sort_by, $sort_order)
+            ->skip(($page - 1) * $page_size)
+            ->take($page_size)
+            ->get();
+
 
         /** Filtrado de los registros nulos y retirado de los existencias  de auditorias no guardadas */
         $query = $query->filter(function ($registro) {
-            if( is_null($registro->existencia_guardada) || is_null($registro->fecha_existencia) ) {
+            if (is_null($registro->existencia_guardada) || is_null($registro->fecha_existencia)) {
                 unset($registro->existencia_guardada);
                 unset($registro->fecha_existencia);
                 unset($registro->id_auditoria_existencia);
                 unset($registro->auditoria_autor);
             }
 
-            if( is_null($registro->existencia_actual) ) {
+            if (is_null($registro->existencia_actual)) {
                 unset($registro->existencia_actual);
             }
 
             return true;
         });
-        
 
-       return self::queryOk($query);
+
+        return self::queryOk($query);
     }
 
     /**
@@ -186,8 +177,8 @@ class ActivosController extends Controller
     {
         AuthController::validateCredentials($request);
         $query = Activo::select()
-                ->get()
-                ->find($id);
+            ->get()
+            ->find($id);
 
         return self::queryOk($query);
     }
@@ -244,11 +235,11 @@ class ActivosController extends Controller
         */
 
         return DB::table('movimientos')
-                    ->join('movimiento_detalle', 'movimientos.idMovimiento', '=', 'movimiento_detalle.idMovimiento')
-                    ->select('*')
-                    ->where('movimiento_detalle.idActivoFijo', $idActivoFijo)
-                    ->orderBy('fecha_acepta', 'desc')
-                    ->take(1)
-                    ->get();
+            ->join('movimiento_detalle', 'movimientos.idMovimiento', '=', 'movimiento_detalle.idMovimiento')
+            ->select('*')
+            ->where('movimiento_detalle.idActivoFijo', $idActivoFijo)
+            ->orderBy('fecha_acepta', 'desc')
+            ->take(1)
+            ->get();
     }
 }
